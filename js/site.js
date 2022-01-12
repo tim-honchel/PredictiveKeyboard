6382aa57788fdb50f5b5933c6ab07d1e49f2245c
Tim Honchel <tim.honchel@gmail.com>
2022-01-12 10:49:13 -0500

    first commit

---
+++ js/site.js
@@ -0,0 +1,44 @@
+﻿// Global variables
+var cursorPosition = 4;
+var resultPosition = 0;
+var searchString = "";
+
+// Events
+
+// On startup
+window.onload = function() {setStartingVariables;}
+
+// When buttons are clicked
+document.getElementById("buttonUp").onclick = function () { pressButtonUp() };
+
+// Functions
+
+function pressButtonUp() {
+    document.getElementById("buttonUp").style.backgroundColor = "yellow";
+    setTimeout(() => { document.getElementById("buttonUp").style.backgroundColor = "whitesmoke" },250);
+    cursorPosition -= 1;
+    updateCursor();
+}
+
+function setStartingVariables() {
+    
+}
+
+function updateCursor() {
+    switch (cursorPosition) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+    }
+}
